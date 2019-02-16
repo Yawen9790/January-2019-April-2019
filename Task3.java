04e98de3a03b8e68991786cf50512e3966c3d97e
Yawen <43248281+Yawen9790@users.noreply.github.com>
2019-02-16 00:42:52 -0500

    Verified Credit Card

--- Task3.java
+++ Task3.java
@@ -1,43 +1,88 @@
-////import java.text.NumberFormat;
-//import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Task3 {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Loan Amount: ");
-		double loan = input.nextDouble();
+	static Scanner input = new Scanner(System.in);
+	
+	//2
+	public static String getCard() { //string to count digit
+		System.out.print("Enter a credit card number as a long interger: ");	
+		String card = input.next();	
+		return card;
+	}
+	
+	//3
+	public static int single(String pcard) {
+		int ssum = 0, temp = 0;
 		
-		System.out.print("Number of Years: ");
-		int years = input.nextInt();
-		
-		System.out.print("Annual Interest Rate: ");
-		double yrate = input.nextDouble();
-
-		double mrate = yrate / 1200;
-		double mp = loan * mrate / (1 - 1 / Math.pow(1 + mrate, years * 12));
-		
-//      DecimalFormat df = new DecimalFormat("0.00");
-		
-		System.out.printf("Monthly Payment: %.2f\n", mp);// df.format(mp));
-		System.out.printf("Total Payment: %.2f\n", (mp * 12) * years);
-
-		double bal = loan;
-		double pr, interest;
-		System.out.println("\nPayment#     Interest     Principal     Balance");
-		
-		for (int i = 1; i <= years * 12; i++) {
-			interest = mrate * bal;
-			pr = mp - interest;
-			bal -= pr;
-			
-//    	    System.out.print(i + "		" +		interest	pr	bal");
-			//format method
-			System.out.printf("%-13d%-13.2f%-13.2f%.2f\n", i, interest, pr, bal);
+		for (int i = 0; i < pcard.length(); i++) {
+			if((i % 2) == 0) {
+				if((pcard.charAt(i) * 2) < 10) {
+					ssum += 2 * pcard.charAt(i);
+				}
+				else {
+					temp = pcard.charAt(i) * 2 - 9;
+					ssum += temp;					
+				}
+			}
 		}
+		return ssum;
+	}
+	
+	//4
+	public static int odd(String pcard) {
+		int osum = 0;
+		
+		for (int i = 0; i < pcard.length(); i++) {
+			if((i % 2) != 0) {	
+				osum += pcard.charAt(i);
+			}
+		}
+		
+		return osum;
+	}
+	
+	//5
+	public static boolean prefix(String pcard) {	
+		if(pcard.charAt(0)==4 || pcard.charAt(0)==5
+				||pcard.charAt(0)==6 
+				|| (pcard.charAt(0)==3 
+				&& pcard.charAt(0)==7)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//6
+	public static int totalsum(String pcard) {
+		int total = 0;
+		
+		total += single(pcard) + odd(pcard);
+		
+		return total;
+	}
+
+	//7
+	//?
+	
+	//1
+	public static boolean valid(String pcard) {
+		if (prefix(pcard) == true) {
+			if (totalsum(pcard) % 10 == 0) {	
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {	
+		String card = getCard();
+		boolean valid = valid(card);
+		
+		if (valid == true) {
+			System.out.print(card + " is valid");	
+		}
+		else {
+			System.out.print(card + " is invalid");				
+		}		
 	}
 }
-
-
-
